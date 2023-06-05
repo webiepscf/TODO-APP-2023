a19d6f2383cddf4897b4d16343b3467175a14c8a
Pascal Lacroix <webiepscf@gmail.com>
2023-06-05 10:51:42 +0200

    Affichage des tasks du localStorage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log('Script chargé !');
+// Je remplis artificiellement le localStorage.tasks
+// localStorage.tasks = JSON.stringify([
+//     {id: 1, content: "Tâche 1", completed: true},
+//     {id: 2, content: "Tâche 2", completed: false}
+// ]);
+
+
+
+// 1. Initialiser le localstorage
+// tasks -> []
+    if (localStorage.tasks === undefined) {
+        localStorage.tasks = JSON.stringify([]);
+    }
+
+
+// 2. Afficher les tasks dans le DOM
+const ul = document.querySelector(".todo-list");
+
+const tasks = JSON.parse(localStorage.tasks);
+
+tasks.forEach(task => {
+    const li = document.createElement("li");
+    if (task.completed) {
+        li.classList.add('completed');
+    } 
+    li.innerHTML = `
+        <div class="view">
+            <input class="toggle" type="checkbox" ${ task.completed ? 'checked': '' } />
+            <label>${ task.content }</label>
+            <button class="destroy"></button>
+        </div>`;
+  ul.appendChild(li);
+});
