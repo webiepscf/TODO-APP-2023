11bddd17fa84bca8bb274eb0bb9d722e304b73ea
Pascal Lacroix <webiepscf@gmail.com>
2023-06-12 10:18:06 +0200

    VANILLAJS - Ajout d'une task

--- classical_vanilla/js/app.js
+++ classical_vanilla/js/app.js
@@ -4,21 +4,8 @@
 //     {id: 2, content: "Tâche 2", completed: false}
 // ]);
 
-
-
-// 1. Initialiser le localstorage
-// tasks -> []
-    if (localStorage.tasks === undefined) {
-        localStorage.tasks = JSON.stringify([]);
-    }
-
-
-// 2. Afficher les tasks dans le DOM
-const ul = document.querySelector(".todo-list");
-
-const tasks = JSON.parse(localStorage.tasks);
-
-tasks.forEach(task => {
+// {id:xxx, content: 'xx', completed:xxx}
+function getTaskDomElement (task) {
     const li = document.createElement("li");
     if (task.completed) {
         li.classList.add('completed');
@@ -29,5 +16,46 @@ tasks.forEach(task => {
             <label>${ task.content }</label>
             <button class="destroy"></button>
         </div>`;
-  ul.appendChild(li);
+    return li;
+}
+
+// 1. Initialiser le localstorage
+// tasks -> []
+    if (localStorage.tasks === undefined) {
+        localStorage.tasks = JSON.stringify([]);
+    }
+
+// 2. Afficher les tasks dans le DOM
+const ul = document.querySelector(".todo-list");
+const tasks = JSON.parse(localStorage.tasks);
+tasks.forEach(task => {
+  ul.appendChild(getTaskDomElement(task));
+});
+
+// AJOUT D'UNE TÂCHE ------------------------------------------
+// Keyup, enter et que le champ n'est pas vide
+// Créer un li et l'ajouter dans le UL
+// Il va falloir mettre à jour le tableau tasks et le localStorage
+document.querySelector(".new-todo").addEventListener("keyup",function (e){
+    if(e.key === "Enter" && this.value != ''){
+        // 1. Créer un objet littéral
+        const newTask = {
+            id: new Date().valueOf(),
+            content: this.value,
+            completed: false,
+        };
+
+        // 2. Ajouter un li dans le ul (insertBefore)
+        ul.insertBefore(getTaskDomElement(newTask), ul.firstChild);
+        
+        // 3. Ajouter la tâche dans tasks (push)
+        tasks.unshift(newTask);
+
+        // 4. Ecraser le localStorage.tasks avec les tasks
+        //localStorage.setItem('tasks', JSON.stringify(tasks));
+        localStorage.tasks = JSON.stringify(tasks);
+
+        // 5. Vider le champs
+        this.value = '';
+    }
 });
